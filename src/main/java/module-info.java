c309a60f9315628487ae1f28fa72a5de106922c8
Ken1chi77 <enzoekh14@gmail.com>
2026-06-19 16:18:20 -0300

    Atualização do Trabalho 3

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,6 @@ module com.example.trabalho {
     opens com.example.trabalho2 to javafx.fxml;
     exports com.example.trabalho2;
 
-    // Esta linha é crucial para que a TableView funcione sem erros de reflexão
     opens com.example.trabalho2.model to javafx.base;
     exports com.example.trabalho2.model;
 }
