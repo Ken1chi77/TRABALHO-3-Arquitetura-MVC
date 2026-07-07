f4e1b97533298acec00a36a55eac11f6b8f53f91
Ken1chi77 <enzoekh14@gmail.com>
2026-07-07 18:19:31 -0300

    Atualiza

--- src/main/java/util/ConnectionFactory.java
+++ src/main/java/util/ConnectionFactory.java
@@ -8,7 +8,7 @@ public class ConnectionFactory {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/trabalho4";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "ekh231949";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         try {
